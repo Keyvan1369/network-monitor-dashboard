0e8c3ac2278c6ec431aad35715a9c39d2ab8d979
Keyvan Hojabr <keyvanhojabr@gmail.com>
2026-06-17 16:15:45 +0200

    Fetch devices from server side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,52 @@
+import { useEffect, useState } from "react";
+import api from "./services/api";
+
 function App() {
+
+  const [devices, setDevices] = useState([]);
+
+  useEffect(() => {
+
+    api.get("/devices")
+      .then((res) => {
+
+        setDevices(res.data);
+
+      })
+      .catch((err) => {
+
+        console.error(err);
+
+      });
+
+  }, []);
+
   return (
+
     <div>
+
       <h1>Network Monitor Dashboard</h1>
+
+      {devices.map((device) => (
+
+        <div key={device.id}>
+
+          <h3>{device.name}</h3>
+
+          <p>{device.ip}</p>
+
+          <p>Status: {device.status}</p>
+
+          <p>Latency: {device.latency}</p>
+
+          <hr />
+
+        </div>
+
+      ))}
+
     </div>
+
   );
 }
 
