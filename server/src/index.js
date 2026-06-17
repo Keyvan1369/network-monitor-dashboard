5581275ccc6a728673a12862a5fadff749660b2b
Keyvan Hojabr <keyvanhojabr@gmail.com>
2026-06-17 21:41:41 +0200

    Create a Socket Server

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,51 @@
+const http = require("http");
+
+const { Server } = require("socket.io");
+
 const app = require("./app");
 
 const checkDevices = require("./services/ping.service");
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const server = http.createServer(app);
 
+const io = new Server(server, {
 
-  checkDevices();
+  cors: {
 
-  
-  setInterval(checkDevices, 10000);
+    origin: "http://localhost:5173",
+
+    methods: ["GET", "POST"]
+
+  }
+
+});
+
+app.set("io", io);
+
+io.on("connection", (socket) => {
+
+  console.log("Client connected");
+
+  socket.on("disconnect", () => {
+
+    console.log("Client disconnected");
+
+  });
+
+});
+
+server.listen(PORT, () => {
+
+  console.log(`Server running on ${PORT}`);
+
+  checkDevices(io);
+
+  setInterval(() => {
+
+    checkDevices(io);
+
+  }, 10000);
+
 });
