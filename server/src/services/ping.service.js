c24bc6f5628a0d557cd5a3ff5cb6de5b02df5e59
Keyvan Hojabr <keyvanhojabr@gmail.com>
2026-06-17 21:44:20 +0200

    Update the Ping Service

--- server/src/services/ping.service.js
+++ server/src/services/ping.service.js
@@ -1,14 +1,16 @@
 const ping = require("ping");
 const devices = require("../devices");
 
-const checkDevices = async () => {
+const checkDevices = async (io) => {
   console.log("Checking devices...");
 
   for (const device of devices) {
     try {
       const result = await ping.promise.probe(device.ip);
 
-      device.status = result.alive ? "online" : "offline";
+      device.status = result.alive
+        ? "online"
+        : "offline";
 
       device.latency = result.alive
         ? parseFloat(result.time)
@@ -17,12 +19,20 @@ const checkDevices = async () => {
       console.log(
         `${device.name}: ${device.status} (${device.latency} ms)`
       );
+
     } catch (err) {
+
       console.error(err);
 
       device.status = "offline";
+
       device.latency = null;
     }
+  }
+
+  
+  if (io) {
+    io.emit("devicesUpdated", devices);
   }
 };
 
