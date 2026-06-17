7577ace38d29bb9d79911b4e85b05c2b09dd97cf
Keyvan Hojabr <keyvanhojabr@gmail.com>
2026-06-17 22:32:38 +0200

    add socket

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,31 @@
 import { useEffect, useState } from "react";
 import api from "./services/api";
 import DeviceCard from "./components/DeviceCard";
+import socket from "./socket";
 import "./App.css";
 
 function App() {
   const [devices, setDevices] = useState([]);
+
 
   useEffect(() => {
     api
       .get("/devices")
       .then((res) => setDevices(res.data))
       .catch(console.error);
+  }, []);
+
+
+  useEffect(() => {
+    socket.on("devicesUpdated", (updatedDevices) => {
+      console.log("Devices updated:", updatedDevices);
+
+      setDevices(updatedDevices);
+    });
+
+    return () => {
+      socket.off("devicesUpdated");
+    };
   }, []);
 
   const total = devices.length;
@@ -23,60 +38,44 @@ function App() {
 
   return (
     <div className="app">
-
       <div className="overlay"></div>
 
       <header>
-
-        <h1>🌐 Network Monitor</h1>
+        <h1> Network Monitor</h1>
 
         <p>
           Monitor your devices in real time
         </p>
-
       </header>
 
       <section className="stats">
-
         <div className="stat-card">
-
           <h2>{total}</h2>
 
           <span>Total Devices</span>
-
         </div>
 
         <div className="stat-card">
-
           <h2>{online}</h2>
 
-          <span>🟢 Online</span>
-
+          <span> Online</span>
         </div>
 
         <div className="stat-card">
-
           <h2>{offline}</h2>
 
-          <span>🔴 Offline</span>
-
+          <span> Offline</span>
         </div>
-
       </section>
 
       <section className="devices">
-
         {devices.map((device) => (
-
           <DeviceCard
             key={device.id}
             device={device}
           />
-
         ))}
-
       </section>
-
     </div>
   );
 }
